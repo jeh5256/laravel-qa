e52091152b6ae830dee69e3b555360a06008a145
Justin <jeh5256@gmail.com>
2018-11-22 11:56:57 -0600

    Add laravel debug. Updates to users. Add Questions view

--- app/User.php
+++ app/User.php
@@ -32,4 +32,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(Question::class);
     }
+
+    public function getUrlAttribute() 
+    {
+        //return route('question.show', $this->id);
+        return '#';
+    }
 }
