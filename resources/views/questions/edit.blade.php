060501b5fbdf71c775153e993f73840400819089
Justin <jeh5256@gmail.com>
2018-12-02 21:34:19 -0600

    Edit question functionality and combine edit/add forms

--- resources/views/questions/edit.blade.php
+++ resources/views/questions/edit.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">
                     <div class="d-flex align-items-center">
-                        <h2>Ask Questions</h2>
+                        <h2>Edit Question</h2>
                         <div class="ml-auto">
                             <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">
                                 Back to all questions
@@ -16,8 +16,9 @@
                     </div>
                 </div>
                 <div class="card-body">
-                    <form method="POST" action="{{ route('questions.store') }}">
-                        @include('questions._form', ['buttonText' => 'Ask Question'])
+                    <form method="POST" action="{{ route('questions.update', $question->id) }}">
+                        {{ method_field('put') }}
+                        @include('questions._form', ['buttonText' => 'Edit Question'])
                     </form>
                 </div>
             </div>
