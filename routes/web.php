8328e3353a0c267960b0fd996f56276d11b5858d
Justin <jeh5256@gmail.com>
2021-07-18 19:58:53 -0500

    add laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,21 +13,12 @@
 |
 */
 
-Route::get('/', 'QuestionsController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Auth::routes();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-//Questions
-Route::resource('questions', 'QuestionsController')->except('show');
-Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
-Route::resource('questions.answers', 'AnswersController')->except(['create', 'show']);
-Route::post('/questions/{question}/vote', 'VoteQuestionController');
-Route::post('/questions/{question}/favorites', 'FavoritesController@store')->name('questions.favorite');
-Route::delete('/questions/{question}/favorites', 'FavoritesController@destroy')->name('questions.unfavorite');
-
-//Answers
-Route::post('/answers/{answer}/accept', 'AcceptAnswerController')->name('answers.accept');
-Route::post('/answers/{answer}/vote', 'VoteAnswerController');
-
+require __DIR__.'/auth.php';
