3f8af03b249da82d41689f3818be87a762e824e6
justin <jeh5256@gmail.com>
2022-01-01 21:23:14 -0600

    Convert routes to laravel 8

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AnswersController;
+use App\Http\Controllers\FavoritesController;
+use App\Http\Controllers\QuestionsController;
+use App\Http\Controllers\VoteAnswerController;
+use App\Http\Controllers\AcceptAnswerController;
+use App\Http\Controllers\VoteQuestionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,4 +33,18 @@ require __DIR__.'/auth.php';
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/', [QuestionsController::class, 'index']);
+
+//Questions
+Route::resource('questions', QuestionsController::class)->except('show');
+Route::get('/questions/{slug}', [QuestionsController::class, 'show'])->name('questions.show');
+Route::resource('questions.answers', AnswersController::class)->except(['create', 'show']);
+Route::post('/questions/{question}/vote', VoteQuestionController::class);
+Route::post('/questions/{question}/favorites', [FavoritesController::class, 'store'])->name('questions.favorite');
+Route::delete('/questions/{question}/favorites', [FavoritesController::class, 'destroy'])->name('questions.unfavorite');
+
+//Answers
+Route::post('/answers/{answer}/accept', AcceptAnswerController::class)->name('answers.accept');
+Route::post('/answers/{answer}/vote', VoteAnswerController::class);
