e4f8d35294459b4559e4a8597d241006693e679e
justin <jeh5256@gmail.com>
2022-06-01 21:31:37 -0500

    Latest user questions and answer

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -24,11 +24,23 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-10">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        <h3 class="font-bold">Questions</h3>
-                        {{ user}}
-                        <div v-for="question in questions">
+                        <h3 class="font-bold pb-5">Latest Questions</h3>
+                    
+                        <div class="py-8 px-5 mt-5 bg-slate-100 rounded-lg" v-for="question in questions">
                             <h5>{{ question.title }}</h5>
-                            {{ question.body }}
+                            <div class="mt-5 overflow-hidden" v-html="question.body_html" /> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-10">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <h3 class="font-bold pb-5">Latest Answers</h3>
+                    
+                        <div class="py-8 px-5 mt-5 bg-slate-100 rounded-lg" v-for="answer in answers">
+                            <h5>{{ answer.title }}</h5>
+                            <div class="mt-5 overflow-hidden" v-html="answer.body_html" /> 
                         </div>
                     </div>
                 </div>
@@ -39,7 +51,12 @@
 <script>
     export default {
         props: {
-            questions
+            questions: {
+                type: Array
+            },
+            answers: {
+                type: Array
+            }
         }
     }
 </script>
