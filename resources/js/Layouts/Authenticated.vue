194bf96dd288c3fb87c66854a6014eba747acbed
justin <jeh5256@gmail.com>
2022-07-10 19:55:47 -0500

    Change user conditional

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -27,10 +27,10 @@
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
                             <!-- Settings Dropdown -->
                             <div class="ml-3 relative">
-                                <BreezeDropdown align="right" width="48">
-                                    <template #trigger>
+                                <BreezeDropdown align="right" width="48" v-if="$page.props.auth.user">
+                                    <templte #trigger>
                                         <span class="inline-flex rounded-md">
-                                            <button v-if="$page.props.auth.user" type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                            <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                                                 {{ $page.props.auth.user.name }}
 
                                                 <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
@@ -38,14 +38,14 @@
                                                 </svg>
                                             </button>
                                         </span>
-                                    </template>
-
+                                    </templte>
                                     <template #content>
                                         <BreezeDropdownLink :href="route('logout')" method="post" as="button">
                                             Log Out
                                         </BreezeDropdownLink>
                                     </template>
                                 </BreezeDropdown>
+                                <Link :href="route('login')">Login</Link>
                             </div>
                         </div>
 
@@ -66,6 +66,9 @@
                     <div class="pt-2 pb-3 space-y-1">
                         <BreezeResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
                             Dashboard
+                        </BreezeResponsiveNavLink>
+                        <BreezeResponsiveNavLink :href="route('questions')" :active="route().current('questions')">
+                            Questions
                         </BreezeResponsiveNavLink>
                     </div>
 
