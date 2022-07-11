63e3091bc1ba4d5377e9f8672f6447a0fef74cb5
justin <jeh5256@gmail.com>
2022-07-10 19:55:16 -0500

    add font awesome

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,12 @@ require('./bootstrap');
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as farStar} from '@fortawesome/free-regular-svg-icons';
+
+library.add(faStar, farStar);
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -12,6 +18,7 @@ createInertiaApp({
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
+            .component('font-awesome-icon', FontAwesomeIcon)
             .mixin({ methods: { route } })
             .mount(el);
     },
