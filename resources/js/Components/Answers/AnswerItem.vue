fb953dd12b4381631a6cde1fa350637229984937
Justin Hughes <jhughes@cliquestudios.com>
2022-08-10 00:01:31 -0500

    Answer Item scaffolding

---
+++ resources/js/Components/Answers/AnswerItem.vue
@@ -0,0 +1,12 @@
+<template>
+    {{ answer.body }}
+</template>
+
+<script setup>
+    defineProps({
+        'answer' : {
+            required: true,
+            type: Object
+        }
+    });
+</script>
