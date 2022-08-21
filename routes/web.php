1d030983e343d035639a0c7dc08c6acb8ea19ad5
justin <jeh5256@gmail.com>
2022-08-21 00:48:20 -0500

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
+use App\Http\Controllers\AnswersController;
 use App\Http\Controllers\DashboarController;
 use App\Http\Controllers\FavoritesController;
 use App\Http\Controllers\QuestionsController;
+use App\Http\Controllers\VoteAnswerController;
+use App\Http\Controllers\AcceptAnswerController;
 use App\Http\Controllers\VoteQuestionController;
 
 /*
@@ -38,8 +41,15 @@ Route::resource('questions', QuestionsController::class)
     ->names([
         'index' => 'questions',
         'show' => 'questions.show'
+    ])
+    ->parameters([
+        'questions' => 'question:slug'
     ]);
 
 Route::post('/questions/{question}/vote', VoteQuestionController::class)->name('questions.upvote');
-
 Route::post('/questions/{question}/favorites', FavoritesController::class)->name('questions.favorite');
+
+Route::resource('questions.answers', AnswersController::class)->except(['create', 'show']);
+
+Route::post('/answers/{answer}/accept', AcceptAnswerController::class)->name('answers.accept');
+Route::post('/answers/{answer}/vote', VoteAnswerController::class);
