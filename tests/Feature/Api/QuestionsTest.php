6656087ca7c06b18d17db4d5570d14dc196584c1
justin <jeh5256@gmail.com>
2024-04-05 07:16:18 -0500

    WIP

---
+++ tests/Feature/Api/QuestionsTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\Question;
+use App\Models\User;
+
+class QuestionsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_create_a_question()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $question = Question::factory()->make();
+
+        $this->json("POST", "/questions", [
+            'title' => $question->title,
+            'slug' => $question->slug,
+            'body' => $question->body
+        ])
+        ->assertRedirect('questions')
+        ->assertSessionHasNoErrors();
+
+        $this->assertDatabaseHas('questions', [
+            'title' => $question->title,
+            'slug' => $question->slug,
+            'body' => $question->body,
+            'views' => 0,
+            'answers' => 0,
+            'best_answer' => null,
+            'user_id' => $user->id
+        ]);
+    }
+}
