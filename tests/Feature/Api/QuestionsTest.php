fca5a58b513ed51ca44c2a7745fb3d2ab09ca778
justin <jeh5256@gmail.com>
2024-09-22 14:20:56 -0500

    Finish question tests

--- tests/Feature/Api/QuestionsTest.php
+++ tests/Feature/Api/QuestionsTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 use App\Models\Question;
 use App\Models\User;
@@ -11,11 +12,20 @@ class QuestionsTest extends TestCase
 {
     use RefreshDatabase;
 
+    protected User $user;
+
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+        $this->actingAs($this->user);
+    }
+
     /** @test */
     public function can_create_a_question()
     {
-        $this->actingAs($user = User::factory()->create());
-
         $question = Question::factory()->make();
 
         $this->json("POST", "/questions", [
@@ -33,13 +43,15 @@ class QuestionsTest extends TestCase
             'views' => 0,
             'answers_count' => 0,
             'best_answer_id' => null,
-            'user_id' => $user->id
+            'user_id' => $this->user->id
         ]);
     }
 
     /** @test */
     public function must_be_authenicated_to_create_a_question()
     {
+        Auth::logout();
+
         $question = Question::factory()->make();
 
         $this->json("POST", "/questions", [
@@ -53,9 +65,7 @@ class QuestionsTest extends TestCase
     /** @test */
     public function a_user_can_edit_their_question()
     {
-        $this->actingAs($user = User::factory()->create());
-
-        $question = Question::factory(['user_id' => $user->id])->create();
+        $question = Question::factory(['user_id' => $this->user->id])->create();
         $editedQuestion = Question::factory()->make();
 
         $this->patch("/questions/{$question->slug}", [
@@ -70,14 +80,135 @@ class QuestionsTest extends TestCase
             'title' => $editedQuestion->title,
             'slug' => $editedQuestion->slug,
             'body' => $editedQuestion->body,
-            'user_id' => $user->id
+            'user_id' => $this->user->id
         ]);
 
         $this->assertDatabaseMissing('questions', [
             'title' => $question->title,
             'slug' => $question->slug,
             'body' => $question->body,
-            'user_id' => $user->id
+            'user_id' => $this->user->id
         ]);
     }
+
+    /** @test */
+    public function a_user_cant_edit_another_users_question()
+    {
+        $anotherUser = User::factory()->create();
+
+        $question = Question::factory([
+            'user_id' => $anotherUser->id
+        ])->create();
+
+        $this->patch("/questions/{$question->slug}", [
+            'title' => 'updated title',
+            'slug' => 'updated slug',
+            'body' => 'uppdated body'
+        ])
+        ->assertForbidden();
+
+        $this->assertDatabaseHas('questions', [
+            'title' => $question->title,
+            'slug' => $question->slug,
+            'body' => $question->body,
+            'user_id' => $anotherUser->id
+        ]);
+    }
+
+    /** @test */
+    public function a_user_can_upvote_a_question()
+    {
+        $question = Question::factory(['vote_count' => 0])->create();
+
+        $this->json("POST", "/questions/{$question->id}/vote", [
+            'vote' => 1
+        ])
+        ->assertStatus(200)
+        ->assertJson([
+            'message' => 'Question upvoted',
+            'votesCount' => $question->vote_count + 1
+        ]);
+    }
+
+    /** @test */
+    public function a_user_cant_upvote_a_question_more_than_once()
+    {
+        $question = Question::factory(['vote_count' => 0])->create();
+
+        $this->json("POST", "/questions/{$question->id}/vote", [
+            'vote' => 1
+        ]);
+       
+        $this->json("POST", "/questions/{$question->id}/vote", [
+            'vote' => 1
+        ])
+        ->assertJson([
+            'votesCount' => 0
+        ]);
+    }
+
+    /** @test */
+    public function a_user_can_downvote_a_question()
+    {
+        $question = Question::factory(['vote_count' => 0])->create();
+
+        $res = $this->json("POST", "/questions/{$question->id}/vote", [
+            'vote' => -1
+        ])
+        ->assertStatus(200)
+        ->assertJson([
+            'message' => 'Question downvoted',
+            'votesCount' => $question->vote_count - 1
+        ]);
+    }
+
+    /** @test */
+    public function a_user_cant_downvote_a_question_more_than_once()
+    {
+        $question = Question::factory(['vote_count' => 0])->create();
+
+        $this->json("POST", "/questions/{$question->id}/vote", [
+            'vote' => -1
+        ]);
+       
+        $this->json("POST", "/questions/{$question->id}/vote", [
+            'vote' => -1
+        ])
+        ->assertJson([
+            'votesCount' => 0
+        ]);
+    }
+
+    /** @test */
+    public function a_user_can_favorite_a_question()
+    {
+        $question = Question::factory()->create();
+
+        $res = $this->json("POST", "/questions/{$question->id}/favorites")
+            ->assertStatus(204);
+
+        $this->assertEmpty($res->getContent());
+
+        $this->assertDatabaseHas('question_favorites', [
+            'user_id' => $this->user->id,
+            'question_id' => $question->id
+        ]);
+
+    }
+
+     /** @test */
+     public function a_user_can_unfavorite_a_question()
+     {
+         $question = Question::factory()->create();
+ 
+         $question->questionFavorites()->toggle(auth()->user()->id);
+
+         $res = $this->json("POST", "/questions/{$question->id}/favorites");
+
+         $this->assertDatabaseMissing('question_favorites', [
+             'user_id' => $this->user->id,
+             'question_id' => $question->id
+         ]);
+ 
+     }
 }
