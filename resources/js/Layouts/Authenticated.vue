afe64fa117c33cf37859f86b4b185b73a5f0e825
Justin <jeh5256@gmail.com>
2024-12-29 08:23:19 -0500

    Add question dropdown

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -18,9 +18,36 @@
                                 <BreezeNavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </BreezeNavLink>
-                                 <BreezeNavLink :href="route('questions.index')" :active="route().current('questions')">
-                                    Questions
-                                </BreezeNavLink>
+                                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                                    <div class=" relative">
+                                        <BreezeDropdown align="right" width="48">
+                                            <template #trigger>
+                                                <span class="inline-flex rounded-md">
+                                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                                        Questions
+
+                                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                                        </svg>
+                                                    </button>
+                                                </span>
+                                            </template>
+                                            <template #content>
+                                                <BreezeDropdownLink 
+                                                    v-if="$page.props.auth.user"
+                                                    :href="route('questions.create')" 
+                                                    :active="route().current('questions')" 
+                                                    as="button"
+                                                >
+                                                    Create
+                                                </BreezeDropdownLink>
+                                                <BreezeDropdownLink :href="route('questions.index')" :active="route().current('questions')" as="button">
+                                                    View All
+                                                </BreezeDropdownLink>
+                                            </template>
+                                        </BreezeDropdown>
+                                    </div>
+                                </div>
                             </div>
                         </div>
 
