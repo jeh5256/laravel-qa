ceb76c2def313958730a82c2ede578ae3aee6a70
Justin <jeh5256@gmail.com>
2025-08-12 23:07:55 -0400

    Update CK Ediitor

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,8 +7,8 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faStar, faArrowUp, faArrowDown, faCheck } from "@fortawesome/free-solid-svg-icons";
 import { faStar as farStar} from '@fortawesome/free-regular-svg-icons';
 import 'vue-toast-notification/dist/theme-sugar.css';
-import { Ckeditor } from '@ckeditor/ckeditor5-vue';
 import ToastPlugin from 'vue-toast-notification';
+import { CkeditorPlugin } from '@ckeditor/ckeditor5-vue';
 
 library.add(faStar, farStar, faArrowUp, faArrowDown, faCheck);
 
@@ -23,8 +23,8 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(Ckeditor)
             .use(ToastPlugin)
+            .use(CkeditorPlugin)
             .component('font-awesome-icon', FontAwesomeIcon)
             .mixin({ methods: { route } })
             .mount(el);
